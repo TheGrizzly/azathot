8756c8839183795252ba4a88a33e0d3b9807f622
TheGrzzly <marco.rivera.neri@hotmail.com>
2019-08-01 23:15:07 -0500

    Healthz funciton fix

--- controller/status.go
+++ controller/status.go
@@ -4,7 +4,7 @@ import "net/http"
 
 // Healthz checks external dependencies status
 
-func (c *Player) Healthz(w http.ResponseWriter, req *http.Response) {
+func (c *Player) Healthz(w http.ResponseWriter, req *http.Request) {
 	//TODO: Check db Connection
 
 	c.render.Text(w, http.StatusOK, "Friendlies")
